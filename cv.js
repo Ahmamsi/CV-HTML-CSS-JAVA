8c1c391934b6542fc6b12331e888eac466fa7578
Ahmamsi <azizhmamsi3@gmail.com>
2024-11-27 16:22:20 +0100

    cv aziz

---
+++ cv.js
@@ -0,0 +1,5 @@
+document.getElementById('toggle-theme').addEventListener('click', function() {
+    document.body.classList.toggle('sombre-mode');
+    const modeText = document.body.classList.contains('sombre-mode') ? 'Mode Clair' : 'Mode Sombre';
+    this.textContent = modeText;
+});

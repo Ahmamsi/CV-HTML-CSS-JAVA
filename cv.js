f537f1e434fee57636eef91bf643a0f1707c8f8d
Ahmamsi <azizhmamsi3@gmail.com>
2024-12-04 14:49:10 +0100

    Fini

--- cv.js
+++ cv.js
@@ -3,3 +3,5 @@ document.getElementById('toggle-theme').addEventListener('click', function() {
     const modeText = document.body.classList.contains('sombre-mode') ? 'Mode Clair' : 'Mode Sombre';
     this.textContent = modeText;
 });
+
+   
